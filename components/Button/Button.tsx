638122106a6682f8b7778a83901867d49e054929
leo.jaimes <leo777jaimes@gmail.com>
2023-02-13 17:40:41 -0600

    upload

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react"
 import { Button } from '@mui/material';
 
-
+import { styles } from './styles'
+import { theme } from 'theme';
 interface Props {
     children?: React.ReactNode
 }
 export const CustomButton: FC<Props> = ({ children }) => {
     return (
-        <Button>
+        <Button
+            sx={styles.btn(theme)}
+        >
             {children}
         </Button>
     )
---
+++ components/Button/styles.ts
@@ -0,0 +1,8 @@
+import { SxProps, Theme } from "@mui/system";
+
+
+export const styles = {
+    btn: (theme: Theme): SxProps<Theme> | undefined => ({
+
+    })
+}
