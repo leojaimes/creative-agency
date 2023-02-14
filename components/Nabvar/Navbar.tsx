6822884f6b7decda57efd2b616dd0714e9456d30
leo.jaimes <leo777jaimes@gmail.com>
2023-02-13 18:51:09 -0600

    upload

---
+++ components/Nabvar/Navbar.tsx
@@ -0,0 +1,68 @@
+import { Box, Button } from '@mui/material';
+import NextImage from 'next/image';
+import { theme } from 'theme/theme';
+import { styles } from './styles';
+const arrow = '/assets/img/chevron-down.svg'
+
+interface page {
+    title: string;
+    arrow: boolean;
+
+}
+const pages: page[] = [
+    {
+        title: 'Demos',
+        arrow: true,
+
+    },
+    {
+        title: 'Services',
+        arrow: true,
+
+    },
+    {
+        title: 'Pages',
+        arrow: true,
+
+    },
+    {
+        title: 'Portfolio',
+        arrow: true,
+
+    },
+    {
+        title: 'Blog',
+        arrow: true,
+
+    },
+    {
+        title: 'Contact',
+        arrow: false,
+
+    },
+]
+export const Navbar = () => {
+    return (
+        <Box
+            sx={{ flexGrow: 1, display: { lg: 'flex' }, justifyContent: 'flex-end' }}
+        >
+            {
+                pages.map((page, index) => (
+                    <Button
+                        key={index}
+                        sx={styles.btn(theme)}
+                    >
+                        {page.title}
+                        {page.arrow && (
+                            <img src={arrow} style={{ marginLeft: '5px', }} alt="arrow" />
+
+                        )}
+
+                    </Button>
+                )
+                )
+            }
+
+        </Box>
+    )
+}
