5dca43c6438db7d22dd736073c98961b123ee548
leo.jaimes <leo777jaimes@gmail.com>
2023-02-13 19:24:08 -0600

    upload

--- components/Nabvar/styles.ts
+++ components/Nabvar/styles.ts
@@ -4,7 +4,7 @@ import { colorsTemplate } from 'theme'
 export const styles = {
     btnLink: (theme: Theme): SxProps<Theme> | undefined => ({
         display: 'flex',
-
+        marginRight: '44px',
         "fontFamily": "'Montserrat'",
         "fontStyle": "normal",
         "fontWeight": "500",
